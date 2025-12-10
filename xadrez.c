91dc53411b4cc992931fc2333d2bd658f703877a
joaovrosa16-lgtm <joaovrosa16@gmail.com>
2025-12-10 23:12:07 +0000

    Ha terminar

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
+//recursiva referente a torre
+void movertorre(int casas){
+    if(casas > 0){
+        printf("Direita\n");
+        movertorre(casas -1);
+    }
+    }
+
+//recursiva com repetição aninhada referente ao bispo
+void moverbispo(int casas){
+    for(casas; casas > 0;casas -- )
+    {for (casas; casas >0;casas --)
+        {
+            printf("Cima\n");
+        }
+        printf("Direita\n");
+    }
+}
+
+// recursiva referente a rainha
+void moverrainha(int casas){
+if(casas > 0){
+printf("esquerda\n");
+ moverrainha(casas -1);
+}
+}
+
 int main() {
-int peça,movimento = 0;
+int peça, movimento=5, casas = 5;
 
 // primeiro darei a opção de esolha da peça ao usuario
 printf("Escolha  uma peça\n");
@@ -11,24 +38,21 @@ switch(peça){
 // codigo referente a movimentação da torre
     case 1:
     printf("Você escolheu a torre\n");
-    while (movimento <= 5)
-    {
-        printf("torre andou para a direita\n");
-        movimento ++;
-    }
+     printf("\n");
+    movertorre(casas);
     break;
 
 // codigo referente a movimentação do bispo
     case 2:
     printf("Você escolheu o bispo\n");
-    do {printf("bispo ando para cima,direita\n");
-        movimento ++;
-    }while(movimento <=5);
+     printf("\n");
+    moverbispo(casas);
     break;
 
     // codigo referent a movimentação do cavalo
     case 3:
     printf("Você escolheu o cavalo\n");
+     printf("\n");
     int movimentodocavalo;
     
     for(movimentodocavalo = 0 ;movimentodocavalo <=2; movimentodocavalo++)
@@ -44,8 +68,8 @@ switch(peça){
     // codigo referente a movimentação da rainha
     default:
     printf("você escolheu a rainha\n");
-    for(movimento = 0;movimento <= 8;movimento ++)
-    {printf("rainha andou 8 casas para a esquerda\n");}
+     printf("\n");
+    moverrainha(casas =8);
     break;
 }
 printf("\n");
